4d7a4bc2664c5a2ff7134e4d69bdd8e57c2fc3b9
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-03 07:59:00 -0300

    Criação do projeto e alteração da fonte para Roboto dentro de globals.css

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <h1>Meu projeto</h1>
+    </>
+  );
+}
