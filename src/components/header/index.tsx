e1f24779fd804f3036f57bf2f2dff768c894fc06
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-03 09:06:11 -0300

    Adicionando header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from './styles.module.css'
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <section className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>
+                            Tarefas<span>+</span>
+                        </h1>
+                    </Link>
+                    <Link href="/dashboard" className={styles.link}>Meu painel</Link>
+                </nav>
+
+                <button className={styles.loginButton}>Acessar</button>
+            </section>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,39 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/home.module.css";
+import Head from "next/head";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+import heroImg from '../../public/assets/hero.png'
 
 export default function Home() {
   return (
-    <>
-      <h1>Meu projeto</h1>
-    </>
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br /> seus estudos e tarefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
