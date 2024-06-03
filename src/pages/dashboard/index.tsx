3f53d24b41e5c0f907bd8eeaf5b5fdb939eae28b
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-03 16:33:13 -0300

    Adicionamento para que somente usuários logados possam acessar a página Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
+
+import { getSession } from'next-auth/react'
 
 export default function Dashboard() {
     return (
@@ -10,5 +13,22 @@ export default function Dashboard() {
 
             <h1>Página Painel</h1>
         </div>
-    )
+    );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req })
+
+    if(!session?.user){
+        return{
+            redirect: {
+                destination: "/",
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {},
+    };
+};
